331ccd052feb3f0049035b5aab5514e9e2d6891e
f <f@localhost>
2024-12-15 15:21:39 +0100

    cleanup: syntax

--- go.mod
+++ go.mod
@@ -2,7 +2,6 @@ module eae_authenticator
 
 go 1.23.1
 
-require (
-	github.com/golang-jwt/jwt/v4 v4.5.1
-	github.com/joho/godotenv v1.5.1
-)
+require github.com/joho/godotenv v1.5.1
+
+require github.com/golang-jwt/jwt/v5 v5.2.1
--- main.go
+++ main.go
@@ -10,13 +10,13 @@ import (
 	"net/http"
 	"os"
 
-	"github.com/golang-jwt/jwt/v4"
+	"github.com/golang-jwt/jwt/v5"
 	"github.com/joho/godotenv"
 )
 
 type RegisterUserRequest struct {
-	Email    string                 `json:"email"`
-	JsonData map[string]interface{} `json:"jsondata"`
+	Email    string         `json:"email"`
+	JsonData map[string]any `json:"jsondata"`
 }
 
 type LoginRequest struct {
@@ -25,10 +25,10 @@ type LoginRequest struct {
 }
 
 type JWTClaims struct {
-	Email string                 `json:"email"`
-	Role  string                 `json:"role"`
-	Data  map[string]interface{} `json:"data"`
-	ID    string                 `json:"id"`
+	Email string         `json:"email"`
+	Role  string         `json:"role"`
+	Data  map[string]any `json:"data"`
+	ID    string         `json:"id"`
 	jwt.RegisteredClaims
 }
 
@@ -59,9 +59,11 @@ func validateHeaders(r *http.Request) error {
 	if r.Header.Get("x-authenticator-psk") != preSharedKey {
 		return errors.New("invalid PSK")
 	}
+
 	if r.Header.Get("Accept-Profile") != "authenticator" {
 		return errors.New("invalid profile")
 	}
+
 	return nil
 }
 
@@ -82,23 +84,25 @@ func registerUser(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	userPayload := map[string]interface{}{
+	payload, _ := json.Marshal(map[string]any{
 		"email": req.Email,
 		"name":  req.JsonData["first_name"],
 		"apps":  []string{applicationName},
-	}
+	})
 
-	userPayloadJSON, _ := json.Marshal(userPayload)
+	resp, err := http.Post(
+		fmt.Sprintf("%s/auth/sign-up?callbackUrl=%s", resourceWatchURL, callbackURL),
+		"application/json",
+		bytes.NewReader(payload),
+	)
+	defer resp.Body.Close()
 
-	resp, err := http.Post(fmt.Sprintf("%s/auth/sign-up?callbackUrl=%s", resourceWatchURL, callbackURL),
-		"application/json", bytes.NewReader(userPayloadJSON))
 	if err != nil || resp.StatusCode != http.StatusOK {
 		http.Error(w, "Failed to create user on Resource Watch", http.StatusInternalServerError)
 		return
 	}
-	defer resp.Body.Close()
 
-	eaePayload := map[string]interface{}{
+	eaePayload := map[string]any{
 		"email": req.Email,
 		"role":  "guest",
 		"data":  req.JsonData,
@@ -106,23 +110,22 @@ func registerUser(w http.ResponseWriter, r *http.Request) {
 
 	eaePayloadJSON, _ := json.Marshal(eaePayload)
 
-	resp, err = http.Post(fmt.Sprintf("%s/authenticator_user_upsert", eaeAPIURL),
-		"application/json", bytes.NewReader(eaePayloadJSON))
+	resp, err = http.Post(
+		fmt.Sprintf("%s/authenticator_user_upsert", eaeAPIURL),
+		"application/json",
+		bytes.NewReader(eaePayloadJSON),
+	)
+	defer resp.Body.Close()
+
 	if err != nil || resp.StatusCode != http.StatusOK {
 		http.Error(w, "Failed to sync user data to EAE service", http.StatusInternalServerError)
 		return
 	}
-	defer resp.Body.Close()
 
 	fmt.Fprintln(w, "User registered successfully! Please check your email inbox to activate your account.")
 }
 
 func login(w http.ResponseWriter, r *http.Request) {
-	if err := validateHeaders(r); err != nil {
-		http.Error(w, err.Error(), http.StatusUnauthorized)
-		return
-	}
-
 	var req LoginRequest
 	if err := json.NewDecoder(r.Body).Decode(&req); err != nil {
 		http.Error(w, "Invalid request body", http.StatusBadRequest)
@@ -134,13 +137,13 @@ func login(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	eaeURL := fmt.Sprintf("%s/users?email=eq.%s", eaeAPIURL, req.Email)
-	resp, err := http.Get(eaeURL)
+	resp, err := http.Get(fmt.Sprintf("%s/users?email=eq.%s", eaeAPIURL, req.Email))
+	defer resp.Body.Close()
+
 	if err != nil || resp.StatusCode != http.StatusOK {
 		http.Error(w, "Failed to fetch user data from EAE service", http.StatusInternalServerError)
 		return
 	}
-	defer resp.Body.Close()
 
 	authPayload := map[string]string{
 		"email":    req.Email,
@@ -148,19 +151,24 @@ func login(w http.ResponseWriter, r *http.Request) {
 	}
 	authPayloadJSON, _ := json.Marshal(authPayload)
 
-	authResp, err := http.Post(fmt.Sprintf("%s/auth/login", resourceWatchURL),
-		"application/json", bytes.NewReader(authPayloadJSON))
+	authResp, err := http.Post(
+		fmt.Sprintf("%s/auth/login", resourceWatchURL),
+		"application/json",
+		bytes.NewReader(authPayloadJSON),
+	)
+	defer authResp.Body.Close()
+
 	if err != nil || authResp.StatusCode != http.StatusOK {
 		http.Error(w, "Authentication failed with Resource Watch", http.StatusUnauthorized)
 		return
 	}
-	defer authResp.Body.Close()
 
 	token := jwt.NewWithClaims(jwt.SigningMethodHS256, JWTClaims{
 		Email: req.Email,
 		Role:  "guest",
-		Data:  map[string]interface{}{"example": "data"},
+		Data:  map[string]any{"example": "data"},
 	})
+
 	tokenString, err := token.SignedString([]byte(jwtSecretKey))
 	if err != nil {
 		http.Error(w, "Error generating token", http.StatusInternalServerError)
@@ -168,6 +176,7 @@ func login(w http.ResponseWriter, r *http.Request) {
 	}
 
 	w.Header().Set("Content-Type", "application/json")
+
 	json.NewEncoder(w).Encode(map[string]string{"token": tokenString})
 }
 
@@ -189,6 +198,7 @@ func main() {
 	defer listener.Close()
 
 	log.Printf("Server is listening on %s", socketPath)
+
 	if err := http.Serve(listener, mux); err != nil {
 		log.Fatalf("Failed to serve: %v", err)
 	}
